f4b03052bd5cd5c39457553d34a6dd027237da7c
Lionel Mangoua <bobymangoua@gmail.com>
2020-01-06 21:01:39 +0000

    Initial commit

---
+++ intro_to_variable.py
@@ -0,0 +1,4 @@
+testNumber = "1"
+name = input("Enter your name: ")
+print("Hi there " + name + "!")
+print("Boby Love Test #" + testNumber)
---
+++ main.py
@@ -0,0 +1,6 @@
+print("Hello World!")
+testNumber = "1"
+name = input("Enter your name: ")
+print("Hi there " + name + "!")
+print("Boby Love Test #" + testNumber)
+
