7ddc14fdfa265a073d567d799c0947fd2b3c67f2
Lionel Mangoua <bobymangoua@gmail.com>
2020-01-06 21:20:58 +0000

    Concat string to int

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 print("Hello World!")
 testNumber = "1"
+integer = 1
 name = input("Enter your name: ")
 print("Hi there " + name + "!")
 print("Boby Love Test #" + testNumber)
-
+print("Integer number: " + str(integer))
