b23046b7151ec43eb6be3d2297a4e24e382c8497
Lionel Mangoua <bobymangoua@gmail.com>
2020-01-08 15:16:42 +0000

    Programming with numbers.

---
+++ program_with_numbers.py
@@ -0,0 +1,7 @@
+expression1 = (5 + 6) * 2
+expression2 = 6 * 4 - 10 / 5 * 2
+expression3 = ((15 + 5) - 10) / 2
+
+print("expression1 is: ") + str(expression1)
+print("expression2 is: ") + str(expression2)
+print("expression3 is: ") + str(expression3)
