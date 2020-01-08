11b0f46552144f5782107262bd31a8fc0cfdc329
Lionel Mangoua <bobymangoua@gmail.com>
2020-01-08 15:41:07 +0000

    Calculate BMI.

--- program_with_numbers.py
+++ program_with_numbers.py
@@ -5,3 +5,8 @@ expression3 = ((15 + 5) - 10) / 2
 print("expression1 is: ") + str(expression1)
 print("expression2 is: ") + str(expression2)
 print("expression3 is: ") + str(expression3)
+
+weight = float(input("Enter your weight: "))
+height = float(input("Enter your height: "))
+bmi = weight + (height ** 2)
+print("Your BMI is " + str(bmi))
